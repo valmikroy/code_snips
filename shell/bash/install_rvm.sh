5d349a7c6d2c98b34811e2ef92fedc782f8abcf1
Valmik Roy <valmikroy@users.noreply.github.com>
2018-08-20 10:13:35 -0700

    Update install_rvm.sh

--- shell/bash/install_rvm.sh
+++ shell/bash/install_rvm.sh
@@ -1,6 +1,6 @@
 export rvm_ignore_gemrc_issues=1
 gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3
-curl -sSL https://get.rvm.io | bash
+curl -sSL https://get.rvm.io | bash -s  --  --auto-dotfiles
 source  /home/users/abhsawan/.profile
 rvm install 2.4.0
 rvm use 2.4.0 --default
