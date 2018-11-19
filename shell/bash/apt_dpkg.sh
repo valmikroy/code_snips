9d71a8f8c1a8be88221c21f9cbfcd65cbeb757c2
Valmik Roy <valmikroy@users.noreply.github.com>
2018-11-19 14:24:04 -0800

    Create apt_dpkg.sh

---
+++ shell/bash/apt_dpkg.sh
@@ -0,0 +1,13 @@
+# For .deb
+
+# get list of files inside .deb
+dpkg -c <deb file>
+
+
+
+
+
+# For installed debian package
+
+# reinstall debian package 
+sudo apt-get install --reinstall  <pkg name which is already installed on host>
