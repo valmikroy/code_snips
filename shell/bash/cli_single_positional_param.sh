3ac9b3f3041df902acce0295e2f840c765475983
Valmik Roy <valmikroy@users.noreply.github.com>
2018-11-20 15:19:09 -0800

    Update cli_single_positional_param.sh

--- shell/bash/cli_single_positional_param.sh
+++ shell/bash/cli_single_positional_param.sh
@@ -2,19 +2,19 @@
 
 die()
 {
-	local _ret=$2
-	test -n "$_ret" || _ret=1
-	test "$_PRINT_HELP" = yes && print_help >&2
-	echo "$1" >&2
-	exit ${_ret}
+        local _ret=$2
+        test -n "$_ret" || _ret=1
+        test "$_PRINT_HELP" = yes && print_help >&2
+        echo "$1" >&2
+        exit ${_ret}
 }
 
 begins_with_short_option()
 {
-	local first_option all_short_options
-	all_short_options='h'
-	first_option="${1:0:1}"
-	test "$all_short_options" = "${all_short_options/$first_option/}" && return 1 || return 0
+        local first_option all_short_options
+        all_short_options='h'
+        first_option="${1:0:1}"
+        test "$all_short_options" = "${all_short_options/$first_option/}" && return 1 || return 0
 }
 
 
@@ -26,55 +26,41 @@ _arg_input_argument="my_default_positional_argument"
 
 print_help ()
 {
-	printf '\n'
-	printf '%s\n' "The general script's help msg"
-	printf 'Usage: %s <my_input_argument>\n' "$0"
-	printf '\t%s\n' "<my_input_argument>: The argument with a default (default: 'my_default_positional_argument - default')"
-	printf '\n'
+        printf '\n'
+        printf '%s\n' "The general script's help msg"
+        printf 'Usage: %s <my_input_argument>\n' "$0"
+        printf '\t%s\n' "<my_input_argument>: The argument with a default (default: 'my_default_positional_argument - default')"
+        printf '\n'
 }
 
 parse_commandline ()
 {
-	while test $# -gt 0
-	do
-		_key="$1"
-		case "$_key" in
-			-h|--help)
-				print_help
-				exit 0
-				;;
-			-h*)
-				print_help
-				exit 0
-				;;
-			*)
-				_positionals+=("$1")
-				;;
-		esac
-		shift
-	done
+        while test $# -gt 0
+        do
+                _key="$1"
+                case "$_key" in
+                        -h|--help)
+                                print_help
+                                exit 0
+                                ;;
+                        -h*)
+                                print_help
+                                exit 0
+                                ;;
+                        *)
+
+                                test -z $_positionals && _positionals="$1" || die "ERROR: script can not take more than one argument" 1
+                                ;;
+                esac
+                shift
+        done
 }
 
 
-handle_passed_args_count ()
-{
-	test ${#_positionals[@]} -le 1 || _PRINT_HELP=yes die "FATAL ERROR: There were spurious positional arguments --- we expect between 0 and 1, but got ${#_positionals[@]} (the last one was: '${_positionals[*]: -1}')." 1
-}
-
-assign_positional_args ()
-{
-	_positional_names=('' )
-
-	for (( ii = 0; ii < ${#_positionals[@]}; ii++))
-	do
-		eval "${_positional_names[ii]}=\${_positionals[ii]}" || die "Error during argument parsing, possibly an Argbash bug." 1
-	done
-}
 
 parse_commandline "$@"
-handle_passed_args_count
-assign_positional_args
+test -z $_positionals && _positionals=$_arg_input_argument
 
 # arg parsing done
 
-echo "Value of third argument: $_arg_input_argument"
+echo "Value of single positional argument: $_positionals"
