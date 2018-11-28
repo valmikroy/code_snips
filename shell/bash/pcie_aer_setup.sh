0f555389d48429313329f7e86a94a66e76439362
Valmik Roy <valmikroy@users.noreply.github.com>
2018-11-28 09:37:55 -0800

    Update pcie_aer_setup.sh

--- shell/bash/pcie_aer_setup.sh
+++ shell/bash/pcie_aer_setup.sh
@@ -1,5 +1,16 @@
 # PCIe interface has Advance Error reporting which can be used for monitoring to measure errors.
 
+# Basics of lspci
+# device get identified by vendor:device id combo which can be seen as output of following command
+# in first line of every device within sqare bracket
+# example:
+# 03:04.0 PCI bridge [0604]: PLX Technology, Inc. Device [10b5:8796] (rev ab) (prog-if 00 [Normal decode])
+#        Control: I/O+ Mem+ BusMaster+ SpecCycle- MemWINV- VGASnoop- ParErr+ Stepping- SERR+ FastB2B- DisINTx-
+#        Status: Cap+ 66MHz- UDF- FastB2B- ParErr- DEVSEL=fast >TAbort- <TAbort- <MAbort- >SERR- <PERR- INTx-
+sudo lspci  -nn -vvv -xx
+
+
+
 # If device support AER or not can be see through command 
 
 lspci  -vvvv -xxx  -s 03:00.0  | grep -e 'Advanced Error Reporting'
