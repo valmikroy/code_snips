f24388aeeec44d24c155e68653499be05ead79e2
Valmik Roy <valmikroy@users.noreply.github.com>
2018-11-30 10:45:01 -0800

    Create assh.sh

---
+++ shell/ssh/assh.sh
@@ -0,0 +1,14 @@
+# Following function will name window based on host they are logged into
+
+function assh {
+
+        if [ -z $TMUX ]
+        then
+                command ssh -A "$@"
+        else
+                tmux rename-window "$(echo $* | cut -d . -f 1)"
+                command ssh -o ForwardAgent=yes  -o StrictHostKeyChecking=no -o BatchMode=yes  "$@"
+                tmux set-window-option automatic-rename "on" 1>/dev/null
+        fi
+
+}
