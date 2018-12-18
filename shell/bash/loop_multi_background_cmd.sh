361d0881e7cdc1f067f0d25c1ef8f1a813720a6a
Valmik Roy <valmikroy@users.noreply.github.com>
2018-12-17 16:07:05 -0800

    Create loop_multi_background_cmd.sh

---
+++ shell/bash/loop_multi_background_cmd.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+
+STOP=0
+
+trap STOP=1 SIGINT SIGTERM
+
+MAX_COUNT=8
+
+while true
+do
+
+        test  $STOP -gt 0  && break
+        RUN_COUNT=`jobs | wc -l`
+
+        if [ $MAX_COUNT -gt $RUN_COUNT ]
+        then
+                echo "spawning"
+                sleep $(expr $RANDOM % 10) &
+        else
+                echo NOT spawning
+        fi
+done
+
+wait
