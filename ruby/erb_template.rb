5317121d5d455df4d4bdc2bc85a4a271e24ac9a7
Valmik Roy <valmikroy@users.noreply.github.com>
2019-05-03 16:44:10 -0700

    Create erb_template.rb

---
+++ ruby/erb_template.rb
@@ -0,0 +1,47 @@
+#!/usr/bin/env ruby
+
+
+require 'erb'
+
+module Helper
+  class Template
+    include ERB::Util
+    attr_accessor :data, :template
+    def initialize(data, template)
+      @data = data
+      @template = template
+    end
+
+    def render
+      ERB.new(@template, nil, '-').result(binding)
+    end
+
+    def save(file)
+      File.open(file, 'w+') { |f| f.write(render) }
+    end
+  end
+
+        def render_tmpl(tmpl, data)
+                e = Template.new(data, tmpl)
+                e.render
+        end
+
+end
+
+include Helper
+
+
+ERB_STR = %{
+        one   for <%=@data[:one]%>
+        two   for <%=@data[:two]%>
+  three for <%=@data[:three]%>
+}
+
+
+data = {
+  :one => '11111111',
+  :two => '22222222',
+  :three => '3333333',
+}
+
+puts render_tmpl(ERB_STR,data)
