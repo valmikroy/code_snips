00385287d00b2bcb41f0c97503e9eb022db6ce65
Valmik Roy <valmikroy@users.noreply.github.com>
2020-01-15 16:30:35 -0800

    Update loop_file_line_by_line_with_action.sh

--- shell/bash/loop_file_line_by_line_with_action.sh
+++ shell/bash/loop_file_line_by_line_with_action.sh
@@ -4,3 +4,24 @@ do
   echo $line
 
 done < <(cat $FILE | grep blah)
+
+
+
+
+#Mutiline var read
+
+read -r -d '' HOST_LIST <<EOF
+one
+two
+three
+four
+EOF
+
+
+
+
+for H in $HOST_LIST
+do
+
+        echo $H
+done
