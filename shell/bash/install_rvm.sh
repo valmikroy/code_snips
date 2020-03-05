5fa3dade112c51f149ab309757b32a6053b8644e
Valmik Roy <valmikroy@users.noreply.github.com>
2020-03-05 12:13:13 -0800

    Update install_rvm.sh

--- shell/bash/install_rvm.sh
+++ shell/bash/install_rvm.sh
@@ -2,8 +2,8 @@ export rvm_ignore_gemrc_issues=1
 gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3
 curl -sSL https://get.rvm.io | bash -s  --  --auto-dotfiles
 source  /home/users/abhsawan/.profile
-rvm install 2.4.4
-rvm use 2.4.4 --default
+rvm install ruby
+rvm --default use ruby
 gem install bundle
 
 
