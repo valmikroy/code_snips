d820491000df73a8920f5edc40e46ed887b3176e
Valmik Roy <valmikroy@users.noreply.github.com>
2021-12-02 11:37:04 -0800

    Update rspec_single_file.rb

--- ruby/rspec_single_file.rb
+++ ruby/rspec_single_file.rb
@@ -1,6 +1,43 @@
 # Credit https://justincypret.com/blog/running-and-testing-a-single-ruby-file
 require "rspec/autorun"
 
+class GroupingFormatter
+  RSpec::Core::Formatters.register self, :dump_summary, :close, :example_failed, :example_passed
+
+
+  def initialize(output)
+    @output = output
+  end
+
+  def example_failed(notification)
+    e = notification.example
+    @output << "FAILED #{e.full_description}\n"
+  end  
+
+  def example_passed(notification)
+    @output << "OK #{notification.example.full_description}\n"
+  end
+
+  def dump_summary(_notification)
+    @output << ''
+  end
+
+  def close(_notification)
+    @output << "\n"
+  end
+
+
+end
+
+
+RSpec.configure do |c|
+  c.formatter = GroupingFormatter
+  c.color = true
+end
+
+
+
+
 class Dice
 
   def initialize(count = 2)
